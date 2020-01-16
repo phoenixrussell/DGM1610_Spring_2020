7bae62e899a112daa32a2e3dae2f641c4e419f13
Russell Brummer <10454102@my.uvu.edu>
2020-01-15 17:17:23 -0700

    edited my variable script

--- UnityNow2020/Assets/Scripts/Assignments/Variables/Variables.cs
+++ UnityNow2020/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    //Declaration
+    public float amount;
+    public float money;
+    public float cost;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cookies(amount,money,cost);
+       
     }
 
     // Update is called once per frame
@@ -15,4 +21,13 @@ public class Variables : MonoBehaviour
     {
         
     }
+
+    void Cookies(float amount, float money, float cost)
+    {
+        float total;
+        total = money - cost * amount;
+        
+        Debug.Log(total);
+    }
+
 }

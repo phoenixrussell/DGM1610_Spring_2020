739e5e9b9aca49945a0f5b7e22cb576cdbe3b3e1
Russell Brummer <russbrum@gmail.com>
2020-01-22 15:58:47 -0700

    print script 01/22

---
+++ UnityNow2020/Assets/Scenes/BeatlesSong.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatlesSong : MonoBehaviour
+{
+    string hello="Goodbye";
+    string bob="to";
+    string yama = "you";
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(hello+" "+bob+" "+yama);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

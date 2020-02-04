850ef12e3990577fe98a99f41a71a47088546fe4
Russell Brummer <russbrum@gmail.com>
2020-02-03 22:27:38 -0700

    02/03

---
+++ New Unity Project/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Light myLight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            myLight.enabled = true;
+        }
+        else
+        {
+            myLight.enabled = false;
+        }
+    }
+}

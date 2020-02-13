ee3d4274486c60c1ce74e4607b0b546f77824b64
Russell Brummer <russbrum@gmail.com>
2020-02-12 17:20:22 -0700

    02/12

---
+++ UnityNow2020/Assets/Scripts/Assignments/Variables/NewBehaviourScript1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn Manager: MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+public int enemeyIndex;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    if (Input.GetKeyDown(KeyCode.S))
+    {
+        Instantiate(enemyPrefabs[enemyIndex], new Vector3(0, 0, 0), enemyPrefabs[enemyIndex].transform.rotation);
+    }
+
+
+    }
+}
